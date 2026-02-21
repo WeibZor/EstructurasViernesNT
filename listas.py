c0e652fc91a9b92ad5dd1875403f18b7fd6d51bf
WeibZor <ivesmolag@gmail.com>
2026-02-20 19:24:22 -0500

    Feat se realizaron creacion de bicicletas.py, listas.py para creación de código ejemplario para procesos de programación Phyton

---
+++ listas.py
@@ -0,0 +1,24 @@
+# Necesito crear un programa
+# que almacena una lista de 200 notas. 
+
+import random
+notas = []
+for i in range(200):
+    notaSimulada = random.randint(1,5)
+    notas.append(notaSimulada)
+    
+#print(notas)
+#notas.insert(0,80)
+#print(notas) #Pone un número insertado a la fuerza
+#notas.remove(1)
+#print(notas) #Elmina el valor númerico
+#notas.sort(reverse=True)
+#notas.clear() #Borra información
+print(notas) 
+
+# Metodos para manipular o transformar, modificar o administrar listas
+
+    
+
+
+    
